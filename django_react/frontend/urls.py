ac32488a38686f56f74fa66d85fe3966f7976468
andrew-bregman <andrew.bregman@westpoint.edu>
2021-04-25 12:32:42 -0400

    avatars on profile

--- django_react/frontend/urls.py
+++ django_react/frontend/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    #path('', views.index),
-    url('', views.index)
+    path('p/<int:ID>',views.profile),
+    path('', views.index),
+    #url('', views.index)
+
 ]
