9e193a65c6bb74b044a3271e90a2ed7987e7a5d4
Andrew Bregman <68543563+andrew-bregman@users.noreply.github.com>
2021-05-18 11:56:53 -0400

    created APIs for departmnet, clubs, and everything in between

--- django_react/leads/urls.py
+++ django_react/leads/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path('department/<int:id>', views.DepartmentAPI.as_view()),
     path('feed/<int:id>', views.FeedAPI.as_view()),
     path('editP1/<int:id>', views.SetProfile1API.as_view()),
+    path('department/<int:id>', views.DepartmentAPI.as_view()),
+    path('club/<int:id>', views.ClubAPI.as_view()),
+    path('club/<int:id>/members', views.ClubMemberAPI.as_view()),
+    path('department/<int:id>/members', views.depProjectAPI.as_view()),
+    path('department/<int:id>/projects', views.depProjectAPI.as_view()),
+    path('department/<int:id>/clubs', views.ClubDepartmentAPI.as_view()),
+    path('department/<int:id>', views.DepartmentAPI.as_view()),
     path('editP2/<int:id>', views.SetProfile2API.as_view()),
     path('editP3/<int:id>', views.SetProfile3API.as_view()),
     #path('feed/<str:firstName>-<str:lastName>-<int:id>', views.FeedAPI.as_view()),
@@ -15,4 +22,4 @@ urlpatterns = [
     path('project/<int:id>/roles', views.RoleAPI.as_view()),
     path('project/<int:id>/milestones', views.MilestoneAPI.as_view()),
     path('project/<int:id>/members', views.ProjMemberAPI.as_view()),
-]   
+]
